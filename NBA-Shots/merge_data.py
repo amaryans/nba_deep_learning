8c309cc566d952db94cd764132febd24f9286041
amaryans <maryanski.austin@gmail.com>
2022-05-12 12:10:18 -0400

    merge_data.py now has capability to get 2 seconds before shot

--- NBA-Shots/merge_data.py
+++ NBA-Shots/merge_data.py
@@ -16,12 +16,9 @@ seq_data_path = '/Users/amaryans/Documents/school/spring22/cosc424/projects/try2
 # moment data is the description of the live game data with player data as well as ball data
 # Drop duplicates because for some reason some data points are duplicated which slows down searches
 # seq_data is the data of the 3 point shots with the x,y,z of the ball data and the outcome
-moment_data = pd.read_csv(moment_data_path, index_col=1)
+moment_data = pd.read_csv(moment_data_path, index_col = 0)
 moment_data = moment_data.drop_duplicates()
 seq_data = pd.read_csv(seq_data_path)
-
-#print(moment_data)
-#print(seq_data)
 
 # Merging the data here based on game clock x y and z, there could be duplicates of the shots in different games,
 # but the odds of that are so low based on the accuracy of the positional data, again need to drop duplicates just in case
@@ -32,6 +29,24 @@ print(merged)
 shot_start = merged.loc[merged['rankc'] == 1]
 print(shot_start)
 
+# This is how I have figured out how to get the moments before the shot, but right now it is dropping over half of
+# the shots. When using get_loc it returns an array of "FALSE", could be because the game clock is that value at many points
+conv_input = pd.DataFrame()
+for val in shot_start['game_clock']:
+    index = moment_data.index.get_loc(val)
+    if isinstance(index, int):
+        conv_input = conv_input.append(moment_data.iloc[index-60:index])
+    else:
+        for i in range(len(index)):
+            if index[i] == True:
+                #print(i)
+                #print(moment_data['z'].iloc[i])
+                #print(shot_start['z'].loc[shot_start['game_clock'] == val])
+                if moment_data['z'].iloc[i] == shot_start['z'].loc[shot_start['game_clock'] == val].values[0]:
+                    print("holy shit")
+                    conv_input = conv_input.append(moment_data.iloc[i-60:i])
+            else:
+                pass
+        print("next")
 
-
-#merged.to_csv("merged_data.csv", mode='a')
+print(conv_input)
